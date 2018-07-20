56310e5e55d978ff5d5d0e7cd9e0d9f36f963e30
Elisa Ou <you35@wisc.edu>
2018-07-20 17:15:34 -0500

    tracking

--- formantsCandidate.m
+++ formantsCandidate.m
@@ -1,5 +1,5 @@
-function formants = formantsWindow( data_win, Fs, p)
-%formantsWindow find formants for a given window
+function [frqs, bw] = formantsCandidate( data_win, Fs, p)
+%formantsCandidate find formants for a given window
 %   data_win is a pre-cropped window size data;
 %   p is the order of LPC estimator
 
@@ -19,14 +19,6 @@ angz = atan2(imag(rts),real(rts)); %angles
 
 [frqs, indices] = sort(angz.*(Fs/(2*pi)));
 bw = -1/2*(Fs/(2*pi))*log(abs(rts(indices))); % bandwidth radius of the poles
-
-nn = 1;
-for kk = 1:length(frqs)
-    if (frqs(kk) > 90 && bw(kk) <400)
-        formants(nn) = frqs(kk);
-        nn = nn+1;
-    end
-end
 
 end
 
--- justfortest.m
+++
@@ -1,8 +0,0 @@
-segmentlen = 100; %window size
-noverlap = 40; %overlapping size
-NFFT = 128; %sampling points for DFT
-
-figure;
-spectrogram(y,segmentlen,noverlap,NFFT,Fs,'yaxis')
-title('Signal Spectrogram')
-
